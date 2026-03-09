59571a12313d681e615d384f91e43edcc64e32fd
Lucas Gismondi <lucas@gismondigroup.com>
2026-03-09 02:05:46 -0400

    feat: adds to gitignore

--- packages/ui/src/index.ts
+++ packages/ui/src/index.ts
@@ -2,11 +2,11 @@
 export * from "./components";
 
 // Theme
-// export { ThemeProvider, useTheme } from "./theme/web";
-// export type {
-//   ThemePreset,
-//   ThemeTokens,
-//   ThemeColors,
-//   ThemeRadius,
-//   ThemeSpacing,
-// } from "./theme/theme.types";
+export { ThemeProvider, useTheme, defaultPreset, darkPreset } from "./theme/web";
+export type {
+  ThemePreset,
+  ThemeTokens,
+  ThemeColors,
+  ThemeRadius,
+  ThemeSpacing,
+} from "./theme/theme.types";
